f9261ebeba635a07e36dd846c74eaefe0287197a
haappi <74637670+haappi@users.noreply.github.com>
2022-07-22 14:58:15 -0500

    intoital

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.papermc.paperweight.userdev") version "1.3.7"
+    id("xyz.jpenilla.run-paper") version "1.0.6"
+}
+
+group = "io.github.haappi"
+version = "0.1"
+
+var mainClassName = "io.github.haappi.ducksmp.DuckSMP"
+val shade = configurations.create("shade")
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.dmulloy2.net/repository/public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/public/")
+    maven("https://repo.opencollab.dev/maven-snapshots/")
+}
+
+dependencies {
+//    implementation("junit:junit:4.13.2")
+    paperDevBundle("1.19-R0.1-SNAPSHOT")
+    compileOnly("org.geysermc.floodgate:api:2.0-SNAPSHOT")
+    implementation("org.mongodb:mongodb-driver-sync:4.6.0")
+//    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+//    testImplementation("com.github.seeseemelk:MockBukkit-v1.18:2.22.2")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    runServer {
+        minecraftVersion("1.19")
+    }
+
+
+
+//    shadowJar {
+//        // helper function to relocate a package into our package
+//        fun reloc(pkg: String) = relocate(pkg, "io.papermc.paperweight.testplugin.dependency.$pkg")
+//
+//        // relocate cloud and it's transitive dependencies
+//        reloc("cloud.commandframework")
+//        reloc("io.leangen.geantyref")
+//    }
+
+    /*
+    reobfJar {
+      // This is an example of how you might change the output location for reobfJar. It"s recommended not to do this
+      // for a variety of reasons, however it"s asked frequently enough that an example of how to do it is included here.
+      outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
+    }
+     */
+}
---
+++ src/main/java/io/github/haappi/ducksmp/DuckSMP.java
@@ -0,0 +1,17 @@
+package io.github.haappi.ducksmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DuckSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
