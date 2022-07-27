9d4384eb56b2d08914766442c4f891b4e9f6592b
haappi <74637670+haappi@users.noreply.github.com>
2022-07-27 00:19:19 -0500

    literally rob a class from the internet

---
+++ src/main/java/io/github/haappi/ducksmp/utils/GUIUtils.java
@@ -0,0 +1,49 @@
+package io.github.haappi.ducksmp.utils;
+
+import io.github.haappi.ducksmp.DuckSMP;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.geysermc.cumulus.form.SimpleForm;
+import org.geysermc.floodgate.api.FloodgateApi;
+import org.geysermc.floodgate.api.player.FloodgatePlayer;
+
+import java.util.UUID;
+
+public class GUIUtils {
+
+    public static void sendOptInForm(UUID uuid) {
+        FloodgatePlayer player = FloodgateApi.getInstance().getPlayer(uuid);
+        SimpleForm.Builder form = SimpleForm.builder()
+                .title("Feedback form")
+                .content("We're asking for feedback, are you willing to enter some feedback to improve our server?")
+                .button("Yes") // id = 0
+                .button("No"); // id = 1;
+        form.closedOrInvalidResultHandler(response -> {
+            // no response was given
+            response.isClosed();
+            response.isInvalid();
+        });
+
+        form.validResultHandler(response -> {
+            if (response.clickedButtonId() == 0) {
+                Bukkit.getPlayer(uuid).sendMessage("Thank you for your feedback!");
+            } else {
+                Bukkit.getPlayer(uuid).sendMessage("We're sorry to hear that, we'll try to improve our server!");
+            }
+        });
+
+        player.sendForm(form);
+    }
+
+public static void sendOptInJava(UUID uuid) {
+    IconMenu menu = new IconMenu("My Fancy Menu", 54, event -> {
+        event.getPlayer().sendMessage("You have chosen " + event.getName());
+        event.setWillClose(true);
+    }, DuckSMP.getInstance())
+            .setOption(3, new ItemStack(Material.APPLE, 1), "Food", "The food is delicious")
+            .setOption(4, new ItemStack(Material.IRON_SWORD, 1), "Weapon", "Weapons are for awesome people")
+            .setOption(5, new ItemStack(Material.EMERALD, 1), "Money", "Money brings happiness");
+}
+
+}
