627ea41081c132d2d6c3930ab04e5335eef5f229
haappi <74637670+haappi@users.noreply.github.com>
2022-07-27 01:54:14 -0500

    Basic check to see if they're in lifesteal or not

--- src/main/java/io/github/haappi/ducksmp/LifeSteal/signup.java
+++ src/main/java/io/github/haappi/ducksmp/LifeSteal/signup.java
@@ -1,8 +1,12 @@
 package io.github.haappi.ducksmp.LifeSteal;
 
+import io.github.haappi.ducksmp.DuckSMP;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 import static io.github.haappi.ducksmp.utils.GUIUtils.sendOptInForm;
@@ -16,7 +20,11 @@ public class signup extends BukkitCommand {
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
         if (sender instanceof Player player) {
-            sendOptInForm(player);
+            if (player.getPersistentDataContainer().get(new org.bukkit.NamespacedKey(DuckSMP.getInstance(), "claimed_hearts"), PersistentDataType.INTEGER) == null) {
+                sendOptInForm(player);
+            } else {
+                player.sendMessage(Component.text("But, you're already in LifeSteal...", NamedTextColor.RED));
+            }
         }
         return true;
     }
