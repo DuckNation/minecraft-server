daba6ea47c097b7f4e5fcf182e1f8972a4213c37
haappi <74637670+haappi@users.noreply.github.com>
2022-07-31 13:55:47 -0500

    [Critical] lint lmao

--- src/main/java/io/github/haappi/ducksmp/Cosemetics/NameTag/Common.java
+++ src/main/java/io/github/haappi/ducksmp/Cosemetics/NameTag/Common.java
@@ -24,7 +24,6 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
