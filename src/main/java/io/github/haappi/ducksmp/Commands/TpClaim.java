f8e76d5464cc9d4b7000d5b17433c48bf8b685cb
haappi <74637670+haappi@users.noreply.github.com>
2022-08-01 15:36:22 -0500

    <red>what does <bold> this </bold> do?

---
+++ src/main/java/io/github/haappi/ducksmp/Commands/TpClaim.java
@@ -0,0 +1,6 @@
+package io.github.haappi.ducksmp.Commands;
+
+public class TpClaim {
+    // see if player has permission to use this command
+    // member / owner of claim -> tp after 10 seconds
+}
