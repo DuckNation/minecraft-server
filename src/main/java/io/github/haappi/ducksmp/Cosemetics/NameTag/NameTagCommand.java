7a66ecf6567570776c7b3adaed0742629097c111
haappi <74637670+haappi@users.noreply.github.com>
2022-08-02 12:22:33 -0500

    force people to be linked to use /color

--- src/main/java/io/github/haappi/ducksmp/Cosemetics/NameTag/NameTagCommand.java
+++ src/main/java/io/github/haappi/ducksmp/Cosemetics/NameTag/NameTagCommand.java
@@ -1,5 +1,10 @@
 package io.github.haappi.ducksmp.Cosemetics.NameTag;
 
+import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
+import github.scarsz.discordsrv.util.DiscordUtil;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
@@ -21,6 +26,21 @@ public class NameTagCommand extends BukkitCommand {
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
         if (sender instanceof Player player) {
+            String discordId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(player.getUniqueId());
+
+            if (discordId == null) {
+                player.sendMessage(Component.text("You must be linked to a Discord account to use this command,", NamedTextColor.RED));
+                player.sendMessage(Component.text("Link using /discord link", NamedTextColor.YELLOW));
+                return true;
+            }
+
+            User user = DiscordUtil.getJda().getUserById(discordId);
+            if (user == null) {
+                player.sendMessage(Component.text("You must be linked to a Discord account to use this command,", NamedTextColor.RED));
+                player.sendMessage(Component.text("Link using /discord link", NamedTextColor.YELLOW));
+                return true;
+            }
+
             if (!FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) {
                 menu(player, args);
             } else {
