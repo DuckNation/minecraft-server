d6c6e0fd8635a7face54cdc7ea2e9877175dfb5b
haappi <74637670+haappi@users.noreply.github.com>
2022-08-03 14:35:27 -0500

    fix

--- src/main/java/io/github/haappi/ducksmp/Commands/TpClaim.java
+++
@@ -1,6 +0,0 @@
-package io.github.haappi.ducksmp.Commands;
-
-public class TpClaim {
-    // see if player has permission to use this command
-    // member / owner of claim -> tp after 10 seconds
-}
