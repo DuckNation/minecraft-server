7aac059f38331095fb6a771bc69e2501d33c2cfe
haappi <74637670+haappi@users.noreply.github.com>
2022-08-04 02:31:22 -0500

    [CI Skip] fix start

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 
 JAVA="java"
 HOME="/home/minecraft/paper/"
-JAR="server.jar"
+JAR="/home/minecraft/paper/server.jar"
 RAM="28000M"
 FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs"
 
 while true; do
     echo "Starting server..."
-    ${JAVA} -Xmx${RAM} -Xms${RAM} "${FLAGS}" -jar "${HOME}${JAR}"  --nogui
+    ${JAVA} -Xmx${RAM} -Xms${RAM} "${FLAGS}" -jar ${JAR}  --nogui
     for i in 3 2 1; do
         printf 'Server restarting in %s... (press CTRL-C to exit)\n' "${i}"
         sleep 1
