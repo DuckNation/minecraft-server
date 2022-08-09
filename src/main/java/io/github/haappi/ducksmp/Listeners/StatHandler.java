ca6f794648c1b42f8b0441faaf0531353414fb00
haappi <74637670+haappi@users.noreply.github.com>
2022-08-09 17:35:03 -0500

    prevent certain item types from gaining stats

--- src/main/java/io/github/haappi/ducksmp/Listeners/StatHandler.java
+++ src/main/java/io/github/haappi/ducksmp/Listeners/StatHandler.java
@@ -11,6 +11,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
@@ -24,40 +25,41 @@ public class StatHandler implements Listener {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-    private boolean canBeUsedForStats(Material item) {
+    private boolean cantBeUsedForStats(Material item) {
         String name = item.toString().toLowerCase();
-        return name.contains("shovel") ||
-                name.contains("pickaxe") ||
-                name.contains("axe") ||
-                name.contains("sword") ||
-                name.contains("hoe") ||
-                name.contains("shears") ||
-                name.contains("bow") ||
-                name.contains("fishing rod") ||
-                name.contains("trident") ||
-                name.contains("boots") ||
-                name.contains("leggings") ||
-                name.contains("chestplate") ||
-                name.contains("helmet");
+        return !name.contains("shovel") &&
+                !name.contains("pickaxe") &&
+                !name.contains("axe") &&
+                !name.contains("sword") &&
+                !name.contains("hoe") &&
+                !name.contains("shears") &&
+                !name.contains("bow") &&
+                !name.contains("fishing rod") &&
+                !name.contains("trident") &&
+                !name.contains("boots") &&
+                !name.contains("leggings") &&
+                !name.contains("chestplate") &&
+                !name.contains("helmet");
+    }
+
+    private ItemMeta getItemMeta(ItemStack itemStack) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta == null) {
+            itemMeta = Bukkit.getItemFactory().getItemMeta(itemStack.getType());
+        }
+        return itemMeta;
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        if (!canBeUsedForStats(event.getPlayer().getActiveItem().getType())) {
+        if (cantBeUsedForStats(event.getPlayer().getInventory().getItemInMainHand().getType())) {
             return;
         }
-        ItemMeta itemMeta = event.getPlayer().getActiveItem().getItemMeta();
-        if (itemMeta == null) {
-            return;
-        }
+        ItemMeta itemMeta = getItemMeta(event.getPlayer().getInventory().getItemInMainHand());
         PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-        if (!pdc.has(new NamespacedKey(plugin, "broken_blocks"), PersistentDataType.INTEGER)) {
-            pdc.set(new NamespacedKey(plugin, "broken_blocks"), PersistentDataType.INTEGER, 1);
-        } else {
-            pdc.set(new NamespacedKey(plugin, "broken_blocks"), PersistentDataType.INTEGER, pdc.get(new NamespacedKey(plugin, "broken_blocks"), PersistentDataType.INTEGER) + 1);
-        }
-
-        event.getPlayer().getActiveItem().setItemMeta(itemMeta);
+        NamespacedKey key = new NamespacedKey(plugin, "broken_blocks");
+        pdc.set(key, PersistentDataType.INTEGER, pdc.getOrDefault(key, PersistentDataType.INTEGER, 0) + 1);
+        event.getPlayer().getInventory().getItemInMainHand().setItemMeta(itemMeta);
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -66,20 +68,16 @@ public class StatHandler implements Listener {
             return;
         }
         if (event.getDamager() instanceof Player player) {
-            if (!canBeUsedForStats(player.getActiveItem().getType())) {
+            if (cantBeUsedForStats(player.getInventory().getItemInMainHand().getType())) {
                 return;
             }
-            ItemMeta itemMeta = player.getActiveItem().getItemMeta();
-            if (itemMeta == null) {
-                return;
-            }
+            ItemMeta itemMeta = getItemMeta(player.getInventory().getItemInMainHand());
             PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-            if (!pdc.has(new NamespacedKey(plugin, "damage_dealt"), PersistentDataType.DOUBLE)) {
-                pdc.set(new NamespacedKey(plugin, "damage_dealt"), PersistentDataType.DOUBLE, event.getDamage());
-            } else {
-                pdc.set(new NamespacedKey(plugin, "damage_dealt"), PersistentDataType.DOUBLE, pdc.get(new NamespacedKey(plugin, "damage_dealt"), PersistentDataType.DOUBLE) + event.getDamage());
-            }
-            player.getActiveItem().setItemMeta(itemMeta);
+
+            NamespacedKey key = new NamespacedKey(plugin, "damage_dealt");
+            pdc.set(key, PersistentDataType.DOUBLE, pdc.getOrDefault(key, PersistentDataType.DOUBLE, 0.0) + event.getFinalDamage());
+
+           player.getInventory().getItemInMainHand().setItemMeta(itemMeta);
         }
 
     }
@@ -90,20 +88,16 @@ public class StatHandler implements Listener {
             return;
         }
         if (event.getDamager() instanceof Player player) {
-            if (!canBeUsedForStats(player.getActiveItem().getType())) {
+            if (cantBeUsedForStats(player.getInventory().getItemInMainHand().getType())) {
                 return;
             }
-            ItemMeta itemMeta = player.getActiveItem().getItemMeta();
-            if (itemMeta == null) {
-                return;
-            }
+            ItemMeta itemMeta = getItemMeta(player.getInventory().getItemInMainHand());
+
             PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-            if (!pdc.has(new NamespacedKey(plugin, "player_damage_dealt"), PersistentDataType.DOUBLE)) {
-                pdc.set(new NamespacedKey(plugin, "player_damage_dealt"), PersistentDataType.DOUBLE, event.getDamage());
-            } else {
-                pdc.set(new NamespacedKey(plugin, "player_damage_dealt"), PersistentDataType.DOUBLE, pdc.get(new NamespacedKey(plugin, "player_damage_dealt"), PersistentDataType.DOUBLE) + event.getDamage());
-            }
-            player.getActiveItem().setItemMeta(itemMeta);
+            NamespacedKey key = new NamespacedKey(plugin, "player_damage_dealt");
+
+            pdc.set(key, PersistentDataType.DOUBLE, pdc.getOrDefault(key, PersistentDataType.DOUBLE, 0.0) + event.getFinalDamage());
+           player.getInventory().getItemInMainHand().setItemMeta(itemMeta);
         }
     }
 
@@ -113,45 +107,36 @@ public class StatHandler implements Listener {
             return;
         }
         if (event.getEntity().getKiller() != null) {
-            if (!canBeUsedForStats(event.getEntity().getKiller().getActiveItem().getType())) {
-                return;
-            }
             Player player = event.getEntity().getKiller();
-            ItemMeta itemMeta = player.getActiveItem().getItemMeta();
-            if (itemMeta == null) {
+            if (cantBeUsedForStats(player.getInventory().getItemInMainHand().getType())) {
                 return;
             }
+            ItemMeta itemMeta = getItemMeta(player.getInventory().getItemInMainHand());
+
             PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-            if (!pdc.has(new NamespacedKey(plugin, "mobs_killed"), PersistentDataType.INTEGER)) {
-                pdc.set(new NamespacedKey(plugin, "mobs_killed"), PersistentDataType.INTEGER, 1);
-            } else {
-                pdc.set(new NamespacedKey(plugin, "mobs_killed"), PersistentDataType.INTEGER, pdc.get(new NamespacedKey(plugin, "mobs_killed"), PersistentDataType.INTEGER) + 1);
-            }
-            player.getActiveItem().setItemMeta(itemMeta);
+            NamespacedKey key = new NamespacedKey(plugin, "mobs_killed");
+
+            pdc.set(key, PersistentDataType.INTEGER, pdc.getOrDefault(key, PersistentDataType.INTEGER, 0) + 1);
+           player.getInventory().getItemInMainHand().setItemMeta(itemMeta);
         }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerDeath(EntityDeathEvent event) {
-        if (!(event.getEntity() instanceof Player)) {
+        if (!(event.getEntity() instanceof Player player)) {
             return;
         }
         if (event.getEntity().getKiller() != null) {
-            if (!canBeUsedForStats(event.getEntity().getKiller().getActiveItem().getType())) {
+            if (cantBeUsedForStats(player.getInventory().getItemInMainHand().getType())) {
                 return;
             }
-            Player player = event.getEntity().getKiller();
-            ItemMeta itemMeta = player.getActiveItem().getItemMeta();
-            if (itemMeta == null) {
-                return;
-            }
+            ItemMeta itemMeta = getItemMeta(player.getInventory().getItemInMainHand());
+
             PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-            if (!pdc.has(new NamespacedKey(plugin, "players_killed"), PersistentDataType.INTEGER)) {
-                pdc.set(new NamespacedKey(plugin, "players_killed"), PersistentDataType.INTEGER, 1);
-            } else {
-                pdc.set(new NamespacedKey(plugin, "players_killed"), PersistentDataType.INTEGER, pdc.get(new NamespacedKey(plugin, "players_killed"), PersistentDataType.INTEGER) + 1);
-            }
-            player.getActiveItem().setItemMeta(itemMeta);
+            NamespacedKey key = new NamespacedKey(plugin, "players_killed");
+
+            pdc.set(key, PersistentDataType.INTEGER, pdc.getOrDefault(key, PersistentDataType.INTEGER, 0) + 1);
+           player.getInventory().getItemInMainHand().setItemMeta(itemMeta);
         }
     }
 
