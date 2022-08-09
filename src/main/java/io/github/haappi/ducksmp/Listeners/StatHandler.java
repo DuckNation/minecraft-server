a1ff811479975ce6d0c70f611090643f870731ce
haappi <74637670+haappi@users.noreply.github.com>
2022-08-09 18:10:57 -0500

    make stats handling so much better

--- src/main/java/io/github/haappi/ducksmp/Listeners/StatHandler.java
+++ src/main/java/io/github/haappi/ducksmp/Listeners/StatHandler.java
@@ -57,71 +57,73 @@ public class StatHandler implements Listener {
         return Double.parseDouble(df.format(input));
     }
 
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        if (cantBeUsedForStats(event.getPlayer().getInventory().getItemInMainHand().getType())) {
+    /**
+     * Automatically increments a stat. This handles all the boilerplate of checking for you, and doesn't do anything if it fails.
+     * <p><b>This increases an integer value, not a double value.</b></p>
+
+     @param stack The itemstack to increment the stat of.
+     @param name The name of the stat to increment.
+     **/
+    @SuppressWarnings("SameParameterValue")
+    private void incrementStat(ItemStack stack, String name) {
+        if (cantBeUsedForStats(stack.getType())) {
             return;
         }
-        ItemMeta itemMeta = getItemMeta(event.getPlayer().getInventory().getItemInMainHand());
-        PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-        NamespacedKey key = new NamespacedKey(plugin, "broken_blocks");
-        pdc.set(key, PersistentDataType.INTEGER, pdc.getOrDefault(key, PersistentDataType.INTEGER, 0) + 1);
-        event.getPlayer().getInventory().getItemInMainHand().setItemMeta(itemMeta);
+
+        ItemMeta itemMeta = getItemMeta(stack);
+        NamespacedKey key = new NamespacedKey(plugin, name);
+
+        PersistentDataContainer persistentDataContainer = itemMeta.getPersistentDataContainer();
+        persistentDataContainer.set(key, PersistentDataType.INTEGER, persistentDataContainer.getOrDefault(key, PersistentDataType.INTEGER, 0) + 1);
+
+        stack.setItemMeta(itemMeta);
+    }
+
+    /**
+     * Automatically increments a stat by the supplied double. This handles all the boilerplate of checking for you, and doesn't do anything if it fails.
+     * <p><b>This increases a double value, not an integer value.</b></p>
+
+     @param stack The itemstack to increment the stat of.
+     @param name The name of the stat to increment.
+     **/
+    @SuppressWarnings("SameParameterValue")
+    private void incrementStat(ItemStack stack, String name, Double amount) {
+        if (cantBeUsedForStats(stack.getType())) {
+            return;
+        }
+
+        ItemMeta itemMeta = getItemMeta(stack);
+        NamespacedKey key = new NamespacedKey(plugin, name);
+
+        PersistentDataContainer persistentDataContainer = itemMeta.getPersistentDataContainer();
+        persistentDataContainer.set(key, PersistentDataType.DOUBLE, getRounded(persistentDataContainer.getOrDefault(key, PersistentDataType.DOUBLE, 0.00) + amount));
+
+        stack.setItemMeta(itemMeta);
+    }
+
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        incrementStat(event.getPlayer().getInventory().getItemInMainHand(), "blocks_broken");
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onDamage(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player player) {
-            if (cantBeUsedForStats(player.getInventory().getItemInMainHand().getType())) {
-                return;
-            }
-            ItemMeta itemMeta = getItemMeta(player.getInventory().getItemInMainHand());
-            PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-
-            NamespacedKey key = new NamespacedKey(plugin, "damage_dealt");
-            pdc.set(key, PersistentDataType.DOUBLE, getRounded(pdc.getOrDefault(key, PersistentDataType.DOUBLE, 0.0) + event.getFinalDamage()));
-
-           player.getInventory().getItemInMainHand().setItemMeta(itemMeta);
+         if (event.getDamager() instanceof Player player) {
+            incrementStat(player.getInventory().getItemInMainHand(), "damage_dealt", event.getFinalDamage());
         }
-
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onEntityDeath(EntityDeathEvent event) {
-        if (event.getEntity() instanceof Player) {
-            return;
+        if (event.getEntity() instanceof Player victim) {
+            if (victim.getKiller() != null) {
+                incrementStat(victim.getKiller().getInventory().getItemInMainHand(), "players_killed");
+            }
         }
         if (event.getEntity().getKiller() != null) {
             Player player = event.getEntity().getKiller();
-            if (cantBeUsedForStats(player.getInventory().getItemInMainHand().getType())) {
-                return;
-            }
-            ItemMeta itemMeta = getItemMeta(player.getInventory().getItemInMainHand());
-
-            PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-            NamespacedKey key = new NamespacedKey(plugin, "mobs_killed");
-
-            pdc.set(key, PersistentDataType.INTEGER, pdc.getOrDefault(key, PersistentDataType.INTEGER, 0) + 1);
-           player.getInventory().getItemInMainHand().setItemMeta(itemMeta);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerDeath(EntityDeathEvent event) {
-        if (!(event.getEntity() instanceof Player player)) {
-            return;
-        }
-        if (event.getEntity().getKiller() != null) {
-            if (cantBeUsedForStats(player.getInventory().getItemInMainHand().getType())) {
-                return;
-            }
-            ItemMeta itemMeta = getItemMeta(player.getInventory().getItemInMainHand());
-
-            PersistentDataContainer pdc = itemMeta.getPersistentDataContainer();
-            NamespacedKey key = new NamespacedKey(plugin, "players_killed");
-
-            pdc.set(key, PersistentDataType.INTEGER, pdc.getOrDefault(key, PersistentDataType.INTEGER, 0) + 1);
-           player.getInventory().getItemInMainHand().setItemMeta(itemMeta);
+            incrementStat(player.getInventory().getItemInMainHand(), "mobs_killed");
         }
     }
 
