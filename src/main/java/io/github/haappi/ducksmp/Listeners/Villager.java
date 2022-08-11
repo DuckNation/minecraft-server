6301d0e83fa0cb3a4313ada42f56074b17d013d7
haappi <74637670+haappi@users.noreply.github.com>
2022-08-10 19:43:42 -0500

    Summon a creeper when breeding villagers

--- src/main/java/io/github/haappi/ducksmp/Listeners/Villager.java
+++ src/main/java/io/github/haappi/ducksmp/Listeners/Villager.java
@@ -4,9 +4,13 @@ import io.github.haappi.ducksmp.DuckSMP;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Warden;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
+
+import static io.github.haappi.ducksmp.Utils.Utils.random;
 
 public class Villager implements Listener {
     private final DuckSMP plugin;
@@ -19,9 +23,18 @@ public class Villager implements Listener {
     @EventHandler
     public void onBreed(EntityBreedEvent event) {
         if (event.getEntity().getType() == org.bukkit.entity.EntityType.VILLAGER) {
-            event.setCancelled(true);
-            if (event.getBreeder() != null) {
-                event.getBreeder().sendMessage(Component.text("You can't breed villagers.", NamedTextColor.RED));
+            if (random.nextBoolean()) {
+                event.setCancelled(false);
+                final Component yay = Component.text("Yay! Someone managed to breed a villager without dying!!!").color(NamedTextColor.GREEN);
+                Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(yay));
+
+            } else {
+                event.setCancelled(true);
+                Warden mob = (Warden) event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), EntityType.WARDEN);
+                mob.customName(Component.text("The FBI", NamedTextColor.BLUE));
+                final Component boo = Component.text("Uh oh. Someone was trying to breed a villager, and got a warden sniffing them..").color(NamedTextColor.RED);
+                Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(boo));
+
             }
         }
     }
