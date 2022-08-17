35ce26ebf38e4ca65e3d46787e9ba7c17c10c399
haappi <74637670+haappi@users.noreply.github.com>
2022-08-17 17:50:00 -0500

    mMmmmmMmmmMmmm

--- src/main/java/io/github/haappi/ducksmp/Biomes/NetherNerf.java
+++ src/main/java/io/github/haappi/ducksmp/Biomes/NetherNerf.java
@@ -4,10 +4,14 @@ import io.github.haappi.ducksmp.DuckSMP;
 import org.bukkit.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.ChunkPopulateEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import java.util.HashSet;
+
+import static io.github.haappi.ducksmp.Utils.Utils.random;
 
 public class NetherNerf implements Listener {
 
@@ -21,7 +25,12 @@ public class NetherNerf implements Listener {
 
 
     @EventHandler
-    public void onChunkLoad(ChunkPopulateEvent event) {
+    public void onChunkLoad(ChunkLoadEvent event) {
+        PersistentDataContainer container = event.getChunk().getPersistentDataContainer();
+        if (container.has(new NamespacedKey(plugin, "nerfed"), PersistentDataType.BYTE)) {
+            return;
+        }
+        // set the PDC
         if (!event.getChunk().contains(Material.ANCIENT_DEBRIS.createBlockData())) {
             return;
         }
@@ -29,18 +38,18 @@ public class NetherNerf implements Listener {
         // debris spawns from y values of 7 to 120
         int counter = 1;
 
-        for (int y = 7; y < 120; y = y + 8) { // each worker thread will do 8 y values at a time
+        for (int y = 7; y < 127; y = y + 7) { // each worker thread will do 8 y values at a time
             int finalY = y;
-            scheduler.runTaskLaterAsynchronously(this.plugin, () -> {
-                stoff(snapshot, event.getChunk(), finalY);
-            }, counter * 2L - 4L * counter);
+            scheduler.runTaskLaterAsynchronously(this.plugin, () -> stoff(snapshot, event.getChunk(), finalY), counter * 4L - 3L * counter);
             counter++;
         }
 
+        container.set(new NamespacedKey(plugin, "nerfed"), PersistentDataType.BYTE, (byte) 1);
     }
 
     private void stoff(ChunkSnapshot snapshot, Chunk chunk, int _y) {
         HashSet<Location> locations = new HashSet<>();
+//        HashMap<String, Location> locationHashMap = new HashMap<>();
         final int chunkX = chunk.getX() << 4;
         final int chunkZ = chunk.getZ() << 4;
         for (int x = 0; x < 16; x++) {
@@ -49,17 +58,21 @@ public class NetherNerf implements Listener {
                     if (snapshot.getBlockType(x, y, z) == Material.ANCIENT_DEBRIS) {
                         final Location location = new Location(chunk.getWorld(), x + chunkX, y, z + chunkZ);
                         locations.add(location);
-                        Bukkit.getScheduler().runTaskLater(this.plugin, () -> location.getBlock().setType(Material.NETHERRACK), 10L);
+                        Bukkit.getScheduler().runTaskLater(this.plugin, () -> location.getBlock().setType(Material.NETHERRACK), 2L);
                     }
                 }
             }
         }
 
-        for (Location location : locations) {
-            for (int y = location.getBlockY() + 20; y < 120; y++) {
-                if (isSafeToPlaceBlock(snapshot, location.getBlockX(), y, location.getBlockZ())) {
-                    Bukkit.getScheduler().runTaskLater(this.plugin, () -> location.getBlock().setType(Material.ANCIENT_DEBRIS), 10L);
-                    System.out.println("placed block at " + location.getBlockX() + " " + y + " " + location.getBlockZ());
+        for (Location _location : locations) {
+            int x = _location.getBlockX();
+            int z = _location.getBlockZ();
+            for (int y = _location.getBlockY() + random.nextInt(8, 25); y < 120; y++) {
+                if (isSafeToPlaceBlock(snapshot, x, y, z)) {
+                    Bukkit.getScheduler().runTaskLater(this.plugin, () -> {
+                        _location.getBlock().setType(Material.ANCIENT_DEBRIS);
+                        locations.remove(_location);
+                    }, 10L);
                     break;
                 }
             }
@@ -68,20 +81,20 @@ public class NetherNerf implements Listener {
     }
 
     private boolean isSafeToPlaceBlock(ChunkSnapshot snapshot, int newX, int y, int newZ) {
-        newX = newX / 16;
-        newZ = newZ / 16;
-        System.out.println("newX: " + newX + " newZ: " + newZ);
-        return (snapshot.getBlockType(newX, y, newZ) != Material.AIR)
-                && (snapshot.getBlockType(newX, y + 1, newZ) != Material.AIR)
-                && (snapshot.getBlockType(newX, y - 1, newZ) != Material.AIR)
-                && (snapshot.getBlockType(newX, y, newZ + 1) != Material.AIR)
-                && (snapshot.getBlockType(newX, y, newZ - 1) != Material.AIR)
-                && (snapshot.getBlockType(newX + 1, y, newZ) != Material.AIR)
-                && (snapshot.getBlockType(newX - 1, y, newZ) != Material.AIR)
-                && (snapshot.getBlockType(newX, y, newZ) != Material.ANCIENT_DEBRIS)
-                && (snapshot.getBlockType(newX, y, newZ) != Material.CHEST)
-                && (snapshot.getBlockType(newX, y, newZ) != Material.GOLD_BLOCK)
-                && (snapshot.getBlockType(newX, y, newZ) != Material.NETHER_PORTAL)
-                && (snapshot.getBlockType(newX, y, newZ) != Material.OBSIDIAN);
+        int newXX = newX & 15;
+        int newZZ = newZ & 15;
+        System.out.println("checking " + newXX + " " + y + " " + newZZ);
+        return (snapshot.getBlockType(newXX, y, newZZ) != Material.AIR)
+                && (snapshot.getBlockType(newXX, y + 1, newZZ) != Material.AIR)
+                && (snapshot.getBlockType(newXX, y - 1, newZZ) != Material.AIR)
+//                && (snapshot.getBlockType(newXX, y, newZZ + 1) != Material.AIR)
+//                && (snapshot.getBlockType(newXX, y, newZZ - 1) != Material.AIR)
+//                && (snapshot.getBlockType(newXX + 1, y, newZZ) != Material.AIR)
+//                && (snapshot.getBlockType(newXX - 1, y, newZZ) != Material.AIR)
+                && (snapshot.getBlockType(newXX, y, newZZ) != Material.ANCIENT_DEBRIS)
+                && (snapshot.getBlockType(newXX, y, newZZ) != Material.CHEST)
+                && (snapshot.getBlockType(newXX, y, newZZ) != Material.GOLD_BLOCK)
+                && (snapshot.getBlockType(newXX, y, newZZ) != Material.NETHER_PORTAL)
+                && (snapshot.getBlockType(newXX, y, newZZ) != Material.OBSIDIAN);
     }
 }
