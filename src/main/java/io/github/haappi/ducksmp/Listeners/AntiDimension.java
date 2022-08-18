357285564da5ad9a1d6c22e6956606897b744751
haappi <74637670+haappi@users.noreply.github.com>
2022-08-17 20:08:12 -0500

    Enable nether I guess

--- src/main/java/io/github/haappi/ducksmp/Listeners/AntiDimension.java
+++ src/main/java/io/github/haappi/ducksmp/Listeners/AntiDimension.java
@@ -1,10 +1,15 @@
 package io.github.haappi.ducksmp.Listeners;
 
 import io.github.haappi.ducksmp.DuckSMP;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.world.PortalCreateEvent;
+
+import static io.github.haappi.ducksmp.Utils.Utils.random;
 
 public class AntiDimension implements Listener {
 
@@ -16,6 +21,19 @@ public class AntiDimension implements Listener {
     }
 
     @EventHandler
+    public void onPortalCreate(PortalCreateEvent event) {
+        if (event.getReason() == PortalCreateEvent.CreateReason.FIRE) {
+            int randomInt = random.nextInt(1, 10);
+            if (randomInt != 1) {
+                event.setCancelled(true);
+                if (event.getEntity() != null) {
+                    event.getEntity().sendMessage(Component.text("Damn, your lighter didn't work. Try again.", NamedTextColor.RED));
+                }
+            }
+        }
+    }
+
+    @EventHandler
     public void onEnd(PlayerTeleportEvent event) {
         if (event.getPlayer().isOp()) {
             return;
@@ -23,8 +41,8 @@ public class AntiDimension implements Listener {
         if (event.getCause() == PlayerTeleportEvent.TeleportCause.END_PORTAL) {
             event.setCancelled(true);
         }
-        if (event.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) {
-            event.setCancelled(true);
-        }
+//        if (event.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) {
+//            event.setCancelled(true);
+//        }
     }
 }
